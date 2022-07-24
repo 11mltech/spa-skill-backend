1323bf11055b25cb000d2040ddbc2e9b178cf42e
Luciano Mantovani <lucianomanto@gmail.com>
2022-07-24 17:16:06 -0300

    implementada handle_accept_grant para obtener tokens. cambio DeviceCloud para usar la libreria urllib.Falta guardar tokens en

--- lambda_function.py
+++ lambda_function.py
@@ -15,14 +15,24 @@
 import json
 import math
 import random
+from urllib import response
 import uuid
 import logging
-import requests
 import datetime
+import urllib.request
+import urllib.parse
+from urllib.error import HTTPError
 from datetime import datetime, timezone
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+# TODO: Update with your Client Id for calling the Login with Amazon (LWA) API.
+client_id = "Your Client Id"
+# TODO: Update with your Client Secret for calling the LWA API.
+client_secret = "Your Client Secret"
+# TODO: Update with your Endpoint Id.
+endpoint_id = "device_id"
 
 def lambda_handler(request, context):
 
@@ -60,11 +70,11 @@ def lambda_handler(request, context):
     # Handle the incoming request from Alexa based on the namespace.
     if namespace == 'Alexa.Authorization':
         if name == 'AcceptGrant':
-            # Note: This example code accepts any grant request.
-            # In your implementation, invoke Login With Amazon with the grant code to get access and refresh tokens.
-            grant_code = request['directive']['payload']['grant']['code']
-            grantee_token = request['directive']['payload']['grantee']['token']
-            auth_response = AlexaResponse(namespace='Alexa.Authorization', name='AcceptGrant.Response')
+            response = handle_accept_grant(request)
+            auth_response = AlexaResponse(message_id=response['event']['header']['messageId'], 
+                                namespace=response['event']['header']['namespace'],
+                                name=response['event']['header']['name'],
+                                payload=response['event']['payload'])
             return send_response(auth_response.get())
 
     if namespace == 'Alexa.Discovery':
@@ -120,9 +130,14 @@ class DeviceCloud:
     # Check if user exists in server, using accessToken provided by directive
     def verify_user(self, **kwargs):
         url = self.address + self.endpoints['verify']
-        response = requests.get(url, params={'accessToken': kwargs.get('accessToken', '')})
-        logger.info(f'GET {url} response status code: {response.status_code}')
-        return response.ok
+        values = {'token' : kwargs.get('accessToken', '')}
+        data = urllib.parse.urlencode(values)
+        data = data.encode('ascii') # data should be bytes
+        req = urllib.request.Request(url, data)
+        with urllib.request.urlopen(req) as response:
+            the_page = response.read()
+        logger.info(f'GET {url} response status code: {response.status}')
+        return response.headers
 
 # Make the call to your device cloud for control
 def update_device_state(endpoint_id, state, value):
@@ -266,3 +281,78 @@ class AlexaResponse:
             self.event['payload']['endpoints'] = []
 
         self.event['payload']['endpoints'] = payload_endpoints
+
+def handle_accept_grant(alexa_request):
+    auth_code = alexa_request["directive"]["payload"]["grant"]["code"]
+    message_id = alexa_request["directive"]["header"]["messageId"]
+
+    # The Login With Amazon API for getting access and refresh tokens from an auth code.
+    lwa_token_url = "https://api.amazon.com/auth/o2/token"
+
+    data = urllib.parse.urlencode(
+        {
+            "grant_type": "authorization_code",
+            "code": auth_code,
+            "client_id": client_id,
+            "client_secret": client_secret
+        }
+    ).encode("utf-8")
+
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+    }
+
+    url_request = urllib.request.Request(lwa_token_url, data, headers, "POST")
+
+    try:
+        with urllib.request.urlopen(url_request) as response:
+            """
+            Response will contain the following:
+            - access_token: Used in events and asynchronous responses to directives that you send to the Alexa event gateway.
+            - refresh_token: Used to obtain a new access_token from LWA when this one expires.
+            - token_type: Expected token type is Bearer.
+            - expires_in: Number of seconds until access_token expires (expected to be 3600, or one hour).
+            """
+            lwa_tokens = json.loads(response.read().decode("utf-8"))
+
+            # TODO: Save the LWA tokens in a secure location, such as AWS Secrets Manager.
+            logger.info("Success!")
+            logger.info(f"access_token: {lwa_tokens['access_token']}")
+            logger.info(f"refresh_token: {lwa_tokens['refresh_token']}")
+            logger.info(f"token_type: {lwa_tokens['token_type']}")
+            logger.info(f"expires_in: {lwa_tokens['expires_in']}")
+    except HTTPError as http_error:
+        logger.error(f"An error occurred: {http_error.read().decode('utf-8')}")
+
+        # Build the failure response to send to Alexa
+        response = {
+            "event": {
+                "header": {
+                    "messageId": message_id,
+                    "namespace": "Alexa.Authorization",
+                    "name": "ErrorResponse",
+                    "payloadVersion": "3"
+                },
+                "payload": {
+                    "type": "ACCEPT_GRANT_FAILED",
+                    "message": "Failed to retrieve the LWA tokens from the user's auth code."
+                }
+            }
+        }
+    else:
+        # Build the success response to send to Alexa
+        response = {
+            "event": {
+                "header": {
+                    "namespace": "Alexa.Authorization",
+                    "name": "AcceptGrant.Response",
+                    "messageId": message_id,
+                    "payloadVersion": "3"
+                },
+                "payload": {}
+            }
+        }
+
+    logger.info(f"accept grant response: {json.dumps(response)}")
+
+    return response
